4b57254c371dda925e4855a1da812a45b987fe7b
drarkell23-web <drarkell23@gmail.com>
2025-11-21 19:32:13 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173,
-    host: true
-  },
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true,
+    port: 5173
   }
-})
+});
