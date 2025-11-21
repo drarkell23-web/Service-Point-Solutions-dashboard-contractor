9ff697b63335c382f121b85260d892b3b693f8fa
drarkell23-web <drarkell23@gmail.com>
2025-11-21 21:03:06 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
 import { Routes, Route, Navigate } from "react-router-dom";
+
+// Layout
 import DashboardLayout from "./layouts/DashboardLayout";
+
+// Pages
 import Dashboard from "./pages/Dashboard";
 import Jobs from "./pages/Jobs";
 import Profile from "./pages/Profile";
 import Auth from "./pages/Auth";
 
-function App() {
+export default function App() {
   return (
     <Routes>
+
+      {/* Public login page */}
       <Route path="/login" element={<Auth />} />
+
+      {/* Dashboard Home */}
       <Route
         path="/"
         element={
@@ -17,6 +25,8 @@ function App() {
           </DashboardLayout>
         }
       />
+
+      {/* Jobs page */}
       <Route
         path="/jobs"
         element={
@@ -25,6 +35,8 @@ function App() {
           </DashboardLayout>
         }
       />
+
+      {/* Profile page */}
       <Route
         path="/profile"
         element={
@@ -33,9 +45,9 @@ function App() {
           </DashboardLayout>
         }
       />
+
+      {/* Catch-all redirect */}
       <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   );
 }
-
-export default App;
